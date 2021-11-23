3001a8db85f945f2482845298c6d54f8c4581caa
Kenny Kerr <kenny@kennykerr.ca>
2021-11-23 15:46:39 -0800

    callback

--- crates/deps/gen2/src/callback.rs
+++ crates/deps/gen2/src/callback.rs
@@ -2,9 +2,10 @@ use super::*;
 
 pub fn gen_callback(def: &TypeDef, gen: &Gen) -> TokenStream {
     let name = gen_type_name(def, gen);
-    let signature = def.invoke_method().signature(&[]);
+    let method = def.invoke_method();
+    let signature = method.signature(&[]);
     let return_sig = gen_return_sig(&signature, gen);
-    let cfg = quote! {}; // gen.gen_function_cfg(def.attributes(), &signature);
+    let cfg = gen.method_cfg(&method).0;
 
     let params = signature.params.iter().map(|p| {
         let name = gen_param_name(&p.param);
