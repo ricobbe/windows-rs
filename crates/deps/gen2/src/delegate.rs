6b5061fb331476b9059ebf615d8e464d76db413f
Kenny Kerr <kenny@kennykerr.ca>
2021-11-23 14:36:21 -0800

    generic

--- crates/deps/gen2/src/delegate.rs
+++ crates/deps/gen2/src/delegate.rs
@@ -2,12 +2,7 @@ use super::*;
 
 pub fn gen_delegate(def: &TypeDef, gen: &Gen) -> TokenStream {
     if gen.sys {
-        let name: TokenStream = if def.generics.is_empty() {
-            def.name().into()
-        } else {
-            let name = def.name();
-            name[..name.len() - 2].into()
-        };
+        let name = gen_generic_ident(def.name());
 
         quote! {
             pub type #name = *mut ::core::ffi::c_void;
--- crates/deps/gen2/src/winrt_interface.rs
+++ crates/deps/gen2/src/winrt_interface.rs
@@ -5,12 +5,7 @@ pub fn gen_winrt_interface(def: &TypeDef, gen: &Gen) -> TokenStream {
         if def.is_exclusive() {
             quote! {}
         } else {
-            let name: TokenStream = if def.generics.is_empty() {
-                def.name().into()
-            } else {
-                let name = def.name();
-                name[..name.len() - 2].into()
-            };
+            let name = gen_generic_ident(def.name());
 
             quote! {
                 pub type #name = *mut ::core::ffi::c_void;
