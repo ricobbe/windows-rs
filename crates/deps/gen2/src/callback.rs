468091db8cd95b3194341415b5683e22583c527e
Kenny Kerr <kenny@kennykerr.ca>
2021-11-23 15:52:51 -0800

    callback

--- crates/deps/gen2/src/callback.rs
+++ crates/deps/gen2/src/callback.rs
@@ -5,7 +5,8 @@ pub fn gen_callback(def: &TypeDef, gen: &Gen) -> TokenStream {
     let method = def.invoke_method();
     let signature = method.signature(&[]);
     let return_sig = gen_return_sig(&signature, gen);
-    let cfg = gen.method_cfg(&method).0;
+    let arch_cfg = gen.arch_cfg(def.attributes());
+    let feature_cfg = gen.method_cfg(&method).0;
 
     let params = signature.params.iter().map(|p| {
         let name = gen_param_name(&p.param);
@@ -14,7 +15,8 @@ pub fn gen_callback(def: &TypeDef, gen: &Gen) -> TokenStream {
     });
 
     quote! {
-        #cfg
+        #arch_cfg
+        #feature_cfg
         pub type #name = ::core::option::Option<unsafe extern "system" fn(#(#params),*) #return_sig>;
     }
 }
