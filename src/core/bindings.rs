675d4163ce2fe412ad9ffcf7846715142da0a7f3
Kenny Kerr <kenny@kennykerr.ca>
2021-11-27 09:33:52 -0800

    bindings

--- src/core/bindings.rs
+++ src/core/bindings.rs
@@ -1465,7 +1465,7 @@ pub mod Windows {
             }
             pub const E_NOINTERFACE: ::windows::core::HRESULT = ::windows::core::HRESULT(-2147467262i32 as _);
             pub const E_OUTOFMEMORY: ::windows::core::HRESULT = ::windows::core::HRESULT(-2147024882i32 as _);
-            pub type FARPROC = unsafe extern "system" fn() -> isize;
+            pub type FARPROC = ::core::option::Option<unsafe extern "system" fn() -> isize>;
             #[inline]
             pub unsafe fn GetLastError() -> WIN32_ERROR {
                 #[cfg(windows)]
@@ -1679,7 +1679,7 @@ pub mod Windows {
             }
             impl ::core::convert::From<WIN32_ERROR> for ::windows::core::HRESULT {
                 fn from(value: WIN32_ERROR) -> Self {
-                    Self(if value.0 as i32 <= 0 { value.0 } else { (value.0 & 0x0000_FFFF) | (7 << 16) | 0x8000_0000 })
+                    Self(if value.0 as i32 <= 0 { value.0 } else { (value.0 & 0x0000_FFFF) | (7 << 16) | 0x8000_0000 } as _)
                 }
             }
         }
